df2d8906b244a888f06b35434d167ac604728db0
Mx. Corey Frang <gnarf37@gmail.com>
2021-12-26 03:35:19 -0500

    gitignore draft

--- Day10.cs
+++ Day10.cs
@@ -3,9 +3,27 @@ namespace AoC2019;
 
 class Day10 : Puzzle
 {
-    // public override void Part1()
-    // {
-    // }
+
+    List<Point2D> points = new List<Point2D>();
+
+    public override void Parse(string filename)
+    {
+        base.Parse(filename);
+        points.Clear();
+        for (int y = 0; y < lines.Length; y++)
+        {
+            var line = lines[y];
+            for (int x = 0; x < line.Length; x++)
+            {
+                if (line[x] == '#') points.Add(new Point2D(x, y));
+            }
+        }
+    }
+
+    public override void Part1()
+    {
+        
+    }
 
     // public override void Part2()
     // {
