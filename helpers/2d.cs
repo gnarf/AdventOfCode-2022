d84afb919b268c6b4dd47c62301f0e06637edb5b
Mx. Corey Frang <gnarf37@gmail.com>
2021-12-26 04:28:06 -0500

    2d from daY10

--- helpers/2d.cs
+++ helpers/2d.cs
@@ -1,18 +1,19 @@
 namespace AoC2019;
 
-public class Point2D : IEquatable<Point2D> {
-    public int x {get; init;}
-    public int y {get; init;}
+public class Point2D : IEquatable<Point2D>
+{
+    public int x { get; init; }
+    public int y { get; init; }
 
     public Point2D(int x, int y)
     {
-        this.x=x;
-        this.y=y;
+        this.x = x;
+        this.y = y;
     }
 
     public bool Equals(Point2D? other)
     {
-        return other is not null && x==other.x && y==other.y;
+        return other is not null && x == other.x && y == other.y;
     }
 
     public override bool Equals(object? obj)
@@ -23,7 +24,7 @@ public class Point2D : IEquatable<Point2D> {
 
     public override int GetHashCode()
     {
-        return Tuple.Create(x,y).GetHashCode();
+        return Tuple.Create(x, y).GetHashCode();
     }
 
     public static bool operator ==(Point2D a, Point2D b)
@@ -35,10 +36,15 @@ public class Point2D : IEquatable<Point2D> {
         return !(a?.Equals(b) ?? b is null);
     }
 
+    public static Point2D operator -(Point2D a, Point2D b)
+    {
+        return new Point2D(a.x - b.x, a.y - b.y);
+    }
+
     public int Manhattan(Point2D? other = null)
     {
         other ??= zero;
-        return Math.Abs(x - other.x) + Math.Abs(y - other.y); 
+        return Math.Abs(x - other.x) + Math.Abs(y - other.y);
     }
 
     public override string ToString()
@@ -47,5 +53,5 @@ public class Point2D : IEquatable<Point2D> {
     }
 
 
-    public static readonly Point2D zero = new Point2D(0,0);
+    public static readonly Point2D zero = new Point2D(0, 0);
 }
