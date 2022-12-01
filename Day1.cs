93f3f07663ec5544457602090cf3c7a23c6e1aa7
Mx. Corey Frang <gnarf37@gmail.com>
2022-12-01 00:14:45 -0500

    day1

--- Day1.cs
+++ Day1.cs
@@ -3,11 +3,32 @@ namespace AoC2022;
 
 class Day1 : Puzzle
 {
+    List<long> elves = new List<long>{0};
+
     public override void Part1()
     {
+
+        int y = 0;
+        for (int x=0;x<lines.Count();x++)
+        {
+            if (String.IsNullOrEmpty(lines[x]))
+            {
+                elves.Add(0);
+                ++y;
+            }
+            else
+            {
+                elves[y]+= Convert.ToInt32(lines[x]);
+
+            }
+        }
+        Console.WriteLine(elves.Max());
     }
 
     public override void Part2()
     {
+        elves.Sort();
+        elves.Reverse();
+        Console.WriteLine(elves.Take(3).Sum());
     }
 }
