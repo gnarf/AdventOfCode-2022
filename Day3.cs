d9e4fd7727a3bea6bd2e5b502f51a4db74b2beb6
Mx. Corey Frang <gnarf37@gmail.com>
2022-12-03 00:16:33 -0500

    Day3       -------Part 1--------   -------Part 2-------- Day       Time  Rank  Score       Time  Rank  Score   3   00:05:05   591      0   00:16:01  2560      0

--- Day3.cs
+++ Day3.cs
@@ -3,11 +3,50 @@ namespace AoC2022;
 
 class Day3 : Puzzle
 {
-    // public override void Part1()
-    // {
-    // }
+    public override void Part1()
+    {
+        Console.WriteLine(lines.Select(s =>
+        {
+            var half = s.Length / 2;
+            var first = s.Substring(0, half);
+            var second = s.Substring(half);
+            var used = new HashSet<char>();
+            for(int x=0;x<half; x++)
+            {
+                used.Add(first[x]);
+            }
+            for(int x=0;x<half; x++)
+            {
+                if (used.Contains(second[x]))
+                {
+                    if (second[x] >= 'a' && second[x] <='z') return (int) (second[x]-'a') + 1;
+                    return (int) (second[x] - 'A') + 27;
+                }
+            }
+            return 0;
+            
+        }).Sum());
+    }
 
-    // public override void Part2()
-    // {
-    // }
+    public override void Part2()
+    {  
+        HashSet<char>[] used = new HashSet<char>[3];
+        for (int x=0; x<3; x++) used[x] = new HashSet<char>();
+        
+        Console.WriteLine(lines.Select((s,i) =>
+        {
+            foreach (var c in s) { used[i%3].Add(c); }
+            if (i%3 == 2)
+            {
+                var result = used[0].Intersect(used[1]).Intersect(used[2]).FirstOrDefault();
+                used[0].Clear();
+                used[1].Clear();
+                used[2].Clear();
+                Console.WriteLine(result);
+                if (result >= 'a' && result <= 'z') return (int)(result - 'a')+1;
+                return (int)(result - 'A')+27;
+            }
+            return 0;
+        }).Sum());
+    }
 }
