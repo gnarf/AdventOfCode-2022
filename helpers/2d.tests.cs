52759fd27026a20c9611a43ec244416fb7e40b35
Mx. Corey Frang <gnarf37@gmail.com>
2022-12-09 04:13:12 -0500

    testturnvalues

--- helpers/2d.tests.cs
+++ helpers/2d.tests.cs
@@ -5,7 +5,7 @@ using NUnit.Framework;
 
 public static class Point2DTests
 {
-    public static object[] values => new object[]
+    public static object[] TestTurnValues => new object[]
     {
         new object[] { Point2D.Up, Turn2D.Left, Point2D.Left },
         new object[] { Point2D.Up, Turn2D.Right, Point2D.Right },
@@ -17,6 +17,6 @@ public static class Point2DTests
         new object[] { new Point2D(10,20), Turn2D.Right, new Point2D(-20, 10)},
     };
 
-    [TestCaseSource(nameof(values))]
+    [TestCaseSource(nameof(TestTurnValues))]
     public static void TestTurn(Point2D point, Turn2D turn, Point2D expected) => Assert.AreEqual( expected, point.Turn(turn) );
 }
